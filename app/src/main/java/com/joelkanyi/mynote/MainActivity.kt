0ad23d949546a30a790d80e408ef68bae4878d54
JoelKanyi <joelkanyi98@gmail.com>
2023-03-17 08:14:16 +0300

    initial

---
+++ app/src/main/java/com/joelkanyi/mynote/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.joelkanyi.mynote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.joelkanyi.mynote.ui.theme.MyNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNoteTheme {
+
+            }
+        }
+    }
+}
